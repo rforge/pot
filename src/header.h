19b3cedc40fe4eb511c701285870ce60aea95577
ribatema <ribatema@80a141b9-e137-0410-8b51-c7aaed2cc172>
2006-10-01 22:37:08 +0000

    Add an header entry for Bivariate POT models
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/pot@122 80a141b9-e137-0410-8b51-c7aaed2cc172

--- src/header.h
+++ src/header.h
@@ -20,3 +20,32 @@ void clust(int *n, double *obs, double *tim, double *cond,
 void ts2tsd(double *time, double *obs, double *start,
 	    double *end, double *obsIntStart, double *obsIntEnd,
 	    int *n, double *ans); 
+
+//From bvgpdlik.c
+void gpdbvlog(double *data1, double *data2, int *n, double *lambda1,
+	      double *lambda2, double *thresh, double *scale1,
+	      double *shape1, double *scale2, double *shape2,
+	      double *alpha, double *dns);
+void gpdbvalog(double *data1, double *data2, int *n, double *lambda1,
+	       double *lambda2, double *thresh, double *scale1,
+	       double *shape1, double *scale2, double *shape2,
+	       double *alpha, double *asCoef1, double *asCoef2,
+	       double *dns);
+void gpdbvnlog(double *data1, double *data2, int *n, double *lambda1,
+	       double *lambda2, double *thresh, double *scale1,
+	       double *shape1, double *scale2, double *shape2,
+	       double *alpha, double *dns);
+void gpdbvanlog(double *data1, double *data2, int *n, double *lambda1,
+		double *lambda2, double *thresh, double *scale1,
+		double *shape1, double *scale2, double *shape2,
+		double *alpha, double *asCoef1, double *asCoef2,
+		double *dns);
+void gpdbvmix(double *data1, double *data2, int *n, double *lambda1,
+	      double *lambda2, double *thresh, double *scale1,
+	      double *shape1, double *scale2, double *shape2,
+	      double *alpha, double *dns);
+void gpdbvamix(double *data1, double *data2, int *n, double *lambda1,
+	       double *lambda2, double *thresh, double *scale1,
+	       double *shape1, double *scale2, double *shape2,
+	       double *alpha, double *asCoef, double *dns);
+
