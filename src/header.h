47cccda40971be7148221f906a38b638e0ece78c
ribatema <ribatema@80a141b9-e137-0410-8b51-c7aaed2cc172>
2006-10-18 19:24:50 +0000

    Modification du fichier d'en-t��te
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/pot@157 80a141b9-e137-0410-8b51-c7aaed2cc172

--- src/header.h
+++ src/header.h
@@ -21,6 +21,10 @@ void ts2tsd(double *time, double *obs, double *start,
 	    double *end, double *obsIntStart, double *obsIntEnd,
 	    int *n, double *ans); 
 
+//From cgpdlik.c
+void cgpdlik(double *data, int *n, int *nn, double *lambda, double *loc,
+	     double *scale, double *shape, double *dns);
+
 //From bvgpdlik.c
 void gpdbvlog(double *data1, double *data2, int *n, int *nn, double *lambda1,
 	      double *lambda2, double *thresh, double *scale1,
@@ -49,3 +53,22 @@ void gpdbvamix(double *data1, double *data2, int *n, int *nn, double *lambda1,
 	       double *shape1, double *scale2, double *shape2,
 	       double *alpha, double *asCoef, double *dns);
 
+//From simmc.c
+void rbvlog(int *n, double *dep, double *sim);
+void rbvalog(int *n, double *dep, double *asy, double *sim);
+void rbvnlog(int *n, double *dep, double *sim);
+void rbvanlog(int *n, double *dep, double *asy, double *sim);
+void rbvamix(int *n, double *alpha, double *beta, double *sim);
+
+//From ccop.c
+void ccop(double *m1, double *m2, int *cnd, double *dep,
+	  double *asy1, double *asy2, double *alpha,
+	  double *beta, int *n, int *model, double *ccop);
+double ccbvlog(double m1, double m2, double oldm1, double dep);
+double ccbvalog(double m1, double m2, double oldm1, double dep,
+		double asy1, double asy2);
+double ccbvnlog(double m1, double m2, double oldm1, double dep);
+double ccbvanlog(double m1, double m2, double oldm1, double dep,
+		 double asy1, double asy2);
+double ccbvamix(double m1, double m2, double oldm1, double alpha,
+		double beta);
