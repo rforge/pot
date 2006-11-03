22214776f607c4c6c73dadb7218a0b454617b5c0
ribatema <ribatema@80a141b9-e137-0410-8b51-c7aaed2cc172>
2006-11-03 17:03:55 +0000

    Add a method to plot return level for mcpot objects
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/pot@238 80a141b9-e137-0410-8b51-c7aaed2cc172

--- R/plotMcGPD.R
+++ R/plotMcGPD.R
@@ -1,6 +1,6 @@
 ##The generic function for graphical diagnostic of a Markov chain
 ##pot object
-plot.mcpot <- function(x, npy, mains, which = 1:4,
+plot.mcpot <- function(x, opy, mains, which = 1:4,
                        ask = nb.fig < length(which) &&
                        dev.interactive(), acf.type = "partial",
                        ...){
@@ -8,11 +8,6 @@ plot.mcpot <- function(x, npy, mains, which = 1:4,
   if (!is.numeric(which) || any(which < 1) || any(which > 4)) 
         stop("`which' must be in 1:4")
 
-  if (missing(npy)){
-    warning("Argument ``npy'' is missing. Setting it to 1.")
-    npy <- 1
-  }
-  
   if(missing(mains))
     mains <- c("Auto-correlation Plot", "Pickands' Dependence Function",
                "Bivariate Return Level Plot", "Spectral Density")
@@ -39,8 +34,44 @@ plot.mcpot <- function(x, npy, mains, which = 1:4,
   if (show[2])
     pickdep(x, main = mains[2], ...)
   if (show[3])
-    retlev(x, npy = npy, main = mains[3], points = FALSE, ...)
+    retlev(x, opy = opy, main = mains[3], ...)
   if (show[4])
     specdens(x, main = mains[4], ...)
 
 }
+
+##The return level plot for object of class ``mcpot''
+retlev.mcpot <- function(fitted, opy, exi, main, xlab, ylab,
+                         xlimsup, ...){
+  loc <- fitted$threshold
+  scale <- fitted$param["scale"]
+  shape <- fitted$param["shape"]
+  data <- fitted$data
+  pat <- fitted$pat
+  
+  if (missing(exi))
+    exi <- fitexi(data, loc)$exi
+
+  pot.fun <- function(T){
+    p <- 1 - 1 / T
+    q <- (1 - p^(1/opy/exi)) / pat
+    q <- loc - scale / shape * (1 - q^(-shape))
+    return(q)
+  }
+
+  if (missing(opy)){
+    warning("Argument ``opy'' is missing. Setting it to 365.")
+    opy <- 365
+  }
+  if (missing(main)) main <- 'Return Level Plot'
+  if (missing(xlab)) xlab <- 'Return Period (Years)'
+  if (missing(ylab)) ylab <- 'Return Level'
+  if (missing(xlimsup)) xlimsup <- 500
+
+  eps <- 10^(-3)
+  plot(pot.fun, from = 1 + eps, to = xlimsup, log = "x",
+       xlab = xlab, ylab = ylab, main = main, ...)
+
+  invisible(pot.fun)
+}
+  
