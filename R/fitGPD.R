91fbf18855a9536553d44f0261f8aca321e8252e
ribatet <ribatet@80a141b9-e137-0410-8b51-c7aaed2cc172>
2007-08-17 14:39:40 +0000

    Add a new estimator for the fitgpd function. See method ``mgf''
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/pot@332 80a141b9-e137-0410-8b51-c7aaed2cc172

--- R/fitGPD.R
+++ R/fitGPD.R
@@ -8,6 +8,7 @@
 ##   6) Minimum Density Power Divergence Estimator
 ##   7) Method of Medians Estimator
 ##   8) Likelihood Moment Estimator
+##   9) Maximum Goodness-of-Fit Estimator
 
 ## A generic function for estimate the GPD parameters
 fitgpd <- function(data, threshold, method = "mle", ...){
@@ -19,11 +20,189 @@ fitgpd <- function(data, threshold, method = "mle", ...){
                    'pickands' = gpdpickands(data, threshold),
                    'mdpd' = gpdmdpd(data, threshold, ...),
                    'med' = gpdmed(data, threshold),
-                   'lme' = gpdlme(data, threshold, ...)
+                   'lme' = gpdlme(data, threshold, ...),
+                   'mgf' = gpdmgf(data, threshold, ...)
                    )
   fitted$threshold.call <- threshold.call
   class(fitted) <- c("uvpot","pot")
   return(fitted)
+}
+
+## Maximum goodness-of-fit estimator
+gpdmgf <- function(x, threshold, start, stat, ...,
+                   method = "BFGS", warn.inf = TRUE){
+
+  nn <- length(x)
+  high <- (x > threshold) & !is.na(x)
+  exceed <- as.double(x[high])
+
+  nat <- length(exceed)
+  if (!nat) 
+    stop("no data above threshold")
+
+  if (!(stat %in% c("KS","CM","AD","ADR","ADL","AD2R","AD2L",
+                   "AD2")))
+    stop("`stat' must be one of 'KS','CM','AD','ADR','ADL','AD2R','AD2L', 'AD2'.")
+
+  pat <- nat/nn
+  param <- c("scale", "shape")
+
+  excess <- exceed - threshold
+
+  excess <- sort(excess)
+
+  if(missing(start)) {
+    
+    start <- list(scale = 0, shape = 0)
+    start$scale <- mean(exceed) - min(threshold)
+    
+    start <- start[!(param %in% names(list(...)))]
+    
+  }
+  
+  if(!is.list(start)) 
+    stop("`start' must be a named list")
+  
+  if(!length(start))
+    stop("there are no parameters left to maximize over")
+  
+  if (stat == "KS")
+    fun <- function(scale, shape){
+      if (scale <= 0)
+        1e6
+
+      else
+        1 / 2 / nat + max(abs(pgpd(excess, 0, scale, shape) -
+                              ppoints(nat)))
+    }
+    
+  if (stat == "CM")
+    fun <- function(scale, shape){
+      if (scale <= 0)
+        1e6
+
+      else
+        1/12/nat + sum((pgpd(excess, 0, scale, shape) -
+                        ppoints(nat))^2)
+    }
+
+  if (stat == "AD")
+    fun <- function(scale, shape){
+      if (scale <= 0)
+        1e6
+
+      else
+        -nat - mean((2*1:nat - 1) *
+                    (log(pgpd(excess, 0, scale, shape)) +
+                     log(1 - pgpd(rev(excess), 0, scale, shape))))
+    }
+
+  if (stat == "ADR")
+    fun <- function(scale, shape){
+      if (scale <= 0)
+        1e6
+
+      else
+        nat / 2 - 2 * sum(pgpd(excess, 0, scale, shape)) -
+          mean((2 * 1:nat -1)*log(1 - pgpd(rev(excess), 0, scale, shape)))
+    }
+  
+  if (stat == "ADL")
+    fun <- function(scale, shape){
+      if (scale <= 0)
+        1e6
+
+      else
+        - 3 * nat / 2 + 2 * sum(pgpd(excess, 0, scale, shape)) -
+          mean((2 * 1:nat -1)*log(pgpd(excess, 0, scale, shape)))
+    }
+
+  if (stat == "AD2R")
+    fun <- function(scale, shape){
+      if (scale <= 0)
+        1e6
+
+      else
+        2 * sum(log(1 - pgpd(excess, 0, scale, shape))) +
+          mean((2* 1:nat - 1) / (1 - pgpd(rev(excess), 0, scale, shape)))
+    }
+
+  if (stat == "AD2L")
+    fun <- function(scale, shape){
+      if (scale <= 0)
+        1e6
+
+      else
+        2 * sum(log(pgpd(excess, 0, scale, shape))) +
+          mean((2 * 1:nat - 1) / pgpd(excess, 0, scale, shape))
+    }
+
+  if (stat == "AD2")
+    fun <- function(scale, shape){
+      if (scale <= 0)
+        1e6
+
+      else
+        2 * sum(log(pgpd(excess, 0, scale, shape)) +
+                log(1 - pgpd(excess, 0, scale, shape))) +
+                  mean((2 * 1:nat - 1) / pgpd(excess, 0, scale, shape) +
+                       (2 * 1:nat - 1) /
+                       (1 - pgpd(rev(excess), 0, scale, shape)))
+    }
+
+  nm <- names(start)
+  l <- length(nm)
+  f <- formals(fun)
+  names(f) <- param
+  m <- match(nm, param)
+  
+  if(any(is.na(m))) 
+    stop("`start' specifies unknown arguments")
+  
+  formals(fun) <- c(f[m], f[-m])
+  mgf <- function(p, ...) fun(p, ...)
+  
+  if(l > 1)
+    body(mgf) <- parse(text = paste("fun(", paste("p[",1:l,
+                          "]", collapse = ", "), ", ...)"))
+  
+  fixed.param <- list(...)[names(list(...)) %in% param]
+  
+  if(any(!(param %in% c(nm,names(fixed.param)))))
+    stop("unspecified parameters")
+  
+  start.arg <- c(list(p = unlist(start)), fixed.param)
+  if( warn.inf && do.call("mgf", start.arg) == 1e6 )
+    warning("Maximum goodness-of-fit function is infinite at starting values")
+  
+  opt <- optim(start, mgf, hessian = TRUE, ..., method = method)
+  
+  if (opt$convergence != 0) {
+    warning("optimization may not have succeeded")
+    if(opt$convergence == 1) opt$convergence <- "iteration limit reached"
+  }
+  
+  else opt$convergence <- "successful"
+
+  tol <- .Machine$double.eps^0.5
+    
+  param <- c(opt$par, unlist(fixed.param))
+  scale <- param["scale"]
+  
+  var.thresh <- !all(threshold == threshold[1])
+
+  if (!var.thresh)
+    threshold <- threshold[1]
+
+  std.err <- std.err.type <- corr.mat <- NULL
+  var.cov <- NULL
+
+  list(fitted.values = opt$par, std.err = std.err, std.err.type = std.err.type,
+       var.cov = var.cov, fixed = unlist(fixed.param), param = param,
+       corr = corr.mat, convergence = opt$convergence, counts = opt$counts,
+       message = opt$message, threshold = threshold, nat = nat, pat = pat,
+       data = x, exceed = exceed, scale = scale, var.thresh = var.thresh,
+       type = "MGF")
 }
 
 ##Likelihood moment estimation
