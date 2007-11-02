4189f8a5cd24f3bac54088885d3f34c995cb1708
mathieu_ribatet <mathieu_ribatet@80a141b9-e137-0410-8b51-c7aaed2cc172>
2007-11-02 00:01:53 +0000

    Add a counter
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/pot@386 80a141b9-e137-0410-8b51-c7aaed2cc172

---
+++ www/counter.php
@@ -0,0 +1,9 @@
+<?
+$fichier = fopen("counts.dat","r+");
+$visitea = fgets($fichier,255);
+$visitea++;
+fseek($fichier,0);
+fputs($fichier,$visitea);
+fclose($fichier);
+echo $visitea;
+?>
--- www/index.php
+++ www/index.php
@@ -35,29 +35,30 @@
     <p>
       The POT package aims to provide operational tools to
       analyze <acronym title="Peaks Over Threshold">
-	POT </acronym>. This package relies on
+	POT.</acronym>  This package relies on
       the <acronym title="Extreme Value Theory"> EVT </acronym> to
-      model the tail of any continuous distribution. Tail modeling, in
-      particular POT modeling, is of great importance for many
+      model the tail of any continuous distribution.  Tail modelling, in
+      particular POT mode-ling, is of great importance for many
       financial and environmental applications.
     </p>
     <p>
-      The POT package was first commited
+      The POT package was first committed
       to the <a href="http://www.r-project.org/">
 	<acronyme title="Comprehensive R Archive Network">
 	  CRAN </acronym> </a> in April 2005 and is still in active
       development.The main motivation was to provide practical tools
-      to model high flood flows. However, the strength of the EVT is
-      that results do not depend on the process to be modeled. Thus,
-      one can use the POT package to analyze precipitations, floods,
-      financial times series, earthquake and so on...
+      for probabilistic modelling of high flood flows.  However, the
+      strength of the EVT is that results do not depend on the process
+      to be modelled.  Thus, one can use the POT package to analyze
+      precipitations, floods, financial times series, earthquake and
+      so on...
     </p>
     <h2>Features</h2> The POT package can perform univariate and
     bivariate extreme value analysis; first order Markov chains can
-    also be considered. For instance, the
+    also be considered.  For instance, the
     (univariate) <acronym title="Generalized Pareto Distribution">
       GPD </acronym> is currently fitted using <strong> 18 </strong>
-    estimators. These estimators rely on three different techniques:
+    estimators.  These estimators rely on three different techniques:
     <ul>
       <li>Likelihood maximization:
 	<acronym title="Maximum Likelihood Estimation"> MLE</acronym>,
@@ -77,8 +78,8 @@
       </ul>
       <p>
 	Contrary to the univariate case, there is no finite
-	parametrization to model bivariate exceedances over
-	thresholds. The POT packages allows 6 parametrisation for the
+	parametrisation to model bivariate exceedances over
+	thresholds.  The POT packages allows 6 parametrisation for the
 	bivariate GPD: the logistic, negative logistic and mixed
 	models - with their respective asymmetric counterparts.
       </p>
@@ -87,11 +88,12 @@
 	bivariate GPD for the joint distribution of two consecutive
 	observations.
       </p>
-      <h2>Screenshots</h2>
-      <p align="center"> <img src="images/screenshot.png"/> </p>
+      <h2>Screen shots</h2>
+      <p align="center"> <a href="images/screenshot.png">
+      <img src="images/screenshot.png"/> </a> </p>
       <h2>Manuals</h2> We have written
       a <a href="../pkg/inst/doc/guide.pdf"> package vignette </a> to
-      help new users. This user's guide is a part of the package -
+      help new users.  This user's guide is a part of the package -
       just run <strong> vignette(POT) </strong> once the package is loaded.
       <p>
 	For a quick overview, one can have a look at
@@ -100,9 +102,16 @@
 	i.e. <strong> demo(POT)</strong> and <strong>
 	demo(bvPOT)</strong>.
       </p>
-      <h3>Contact</h3>
-      Any suggestions, features request,
+      <h3>Contact</h3> Any suggestions, features request,
       bugs: <a href="http://r-forge.r-project.org/tracker/?group_id=76">
-      appropriate tracker</a>
+      select the appropriate tracker</a><br/> Author: Mathieu
+      Ribatet <a href="http://www.lyon.cemagref.fr/hh/hydrologie/scientifiques/ribatet/hydrology-english.shtml">
+      (homepage)</a><br/> Date: $Date$
     </body>
+    <p align="center">
+      Visited <?php
+	 include ("counter.php");
+	 ?>
+      times
+    </p>
   </html>
