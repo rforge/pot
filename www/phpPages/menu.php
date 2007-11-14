570a959c2dc24fa59d25bdbaf60f0d07b4f6928f
mathieu_ribatet <mathieu_ribatet@80a141b9-e137-0410-8b51-c7aaed2cc172>
2007-11-14 21:40:52 +0000

    Remove duplicate entry
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/pot@410 80a141b9-e137-0410-8b51-c7aaed2cc172

--- www/phpPages/menu.php
+++ www/phpPages/menu.php
@@ -2,7 +2,6 @@
 <li><a href="#features">Features</a></li>
 <li><a href="#screen_shots">Screen Shots</a></li>
 <li><a href="#pot_few_lines">Short Applications</a></li>
-<li><a href="#screen_shots">Screen Shots</a></li>
 <li><a href="http://r-forge.r-project.org/projects/pot/">Download</a></li>
 <li><a href="#manuals">Manuals</a></li>
 <li><a href="#contact">Contact</a></li>
