c3b521a13b9f6003291bc5285913d8a163406049
mathieu_ribatet <mathieu_ribatet@80a141b9-e137-0410-8b51-c7aaed2cc172>
2007-11-24 13:03:14 +0000

    PNG transparency with IE < 7 again
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/pot@425 80a141b9-e137-0410-8b51-c7aaed2cc172

--- www/index.php
+++ www/index.php
@@ -15,9 +15,24 @@
 	  title="My Design" href="css/mystyle.css" />
     <link rel="shortcut icon" type="image/png" 
 	  href="images/favicon.png" />
-    <!--[if lt IE 7.]>
-	<script defer type="text/javascript" src="javascript/sleight.js"></script>
-	<![endif]-->
+    <script type="text/javascript">
+      function setpng(img) { 
+      if(document.all &&
+      (IEver=parseFloat(navigator.appVersion.split("MSIE")[1])) &&
+      (IEver>=5.5) && (IEver<7) && document.body.filters && img) {
+				imgName=img.src.toUpperCase();
+				if(imgName.substring(imgName.length-3,imgName.length)=="PNG")
+				img.outerHTML=
+      "<span "+(img.id?"id='"+img.id+"' ":"") +
+      (img.className?"class='"+img.className+"' ":"") +
+      (img.title?"title=\""+img.title+"\" ":"") +
+      "style=\"width:"+img.width+"px;height:"+img.height+"px;
+				"+(img.align=="left"?"float:left;":(img.align=="right"?"float:right;":""))
+      +(img.parentElement.href?"cursor:hand;":"")+"display:inline-block;"+img.style.cssText+";"
+      +"filter:progid:DXImageTransform.Microsoft.AlphaImageLoader(src='"+img.src+"',sizingMethod='scale');\"></span>";
+				}
+				}
+</script>
   </head>
   <body>
     <div id="wrapper">
@@ -27,17 +42,21 @@
 	  provided by
 	  <a href="http://r-forge.r-project.org/">
 	    <img src="images/RForgeLogo.png"
-		 width="100%" align="left" alt="RForge Logo" />	    
+		 width="100%" align="left" alt="RForge Logo"
+		 onload="setpng(this)" />	    
 	  </a>   
 	</span>   
 	<div class="mainTitle">
 	  <img src="images/title.png"
 	       height="90px" 
-	       alt="The POT Package" />
+	       alt="The POT Package"
+	       onload="setpng(this)"
+	       />
 	  <br/>
 	  <img src="images/subtitle.png"
 	       height="50px" 
-	       alt="The POT Package" />
+	       alt="The POT Package"
+	       onload="setpng(this)" />
 	</div>
       </div>
       <!-- menu part -->  
--- www/phpPages/screen_shots.php
+++ www/phpPages/screen_shots.php
@@ -2,6 +2,7 @@
   <a href="images/screenshot.png">
     <img src="images/screenshot.png" 
 	 alt="POT screen shots"
-	 width="100%"/> 
+	 width="100%"
+	 onload="setpng(this)" /> 
   </a> 
 </p>
